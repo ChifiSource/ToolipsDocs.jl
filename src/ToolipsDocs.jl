7e2ac69d12eb5f7eb8066db7d40306dc4509a88c
emmy <52672675+emmettgb@users.noreply.github.com>
2022-06-24 21:10:49 -0500

    updated source to properly serve :)

--- src/ToolipsDocs.jl
+++ src/ToolipsDocs.jl
@@ -6,9 +6,36 @@ fourofour = route("404") do c
 end
 
 home = route("/") do c
-    write!(c, read("public/index.html", String))
+    write!(c, File("public/index.html"))
 end
 
+projects = route("/projects/") do c
+    write!(c, File("public/projects/index.html"))
+end
+
+servables = route("/interface/") do c
+    write!(c, File("public/interface/index.html"))
+end
+
+core = route("/core/") do c
+    write!(c, File("public/core/index.html"))
+end
+
+sess = route("/toolips_session/") do c
+    write!(c, File("public/toolips_session/index.html"))
+end
+
+createss = route("/creating_servables/") do c
+    write!(c, File("public/creating_servables/index.html"))
+end
+
+devapi = route("/developer_api/") do c
+    write!(c, File("public/developer_api/index.html"))
+end
+
+search = route("/search/") do c
+    write!(c, File("public/search/index.html"))
+end
 """
 start(IP::String, PORT::Integer, extensions::Vector{Any}) -> ::Toolips.WebServer
 --------------------
@@ -17,7 +44,8 @@ The start function comprises routes into a Vector{Route} and then constructs
 """
 function start(IP::String = "127.0.0.1", PORT::Integer = 8000,
     extensions::Vector = [Logger()])
-    rs = routes(fourofour, home)
+    rs = routes(fourofour, home, search, devapi, createss, sess, core, servables,
+    projects, home)
     server = ServerTemplate(IP, PORT, rs, extensions = extensions)
     server.start()
 end
